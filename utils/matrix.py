79bad13659c82e23ef7ccab4b4242e736b82f7e1
Juan Andrés <you@example.com>
2017-11-13 17:43:36 +0100

    Implementación del módulo utils/matrix.     * Función elements_sum     * Función diagonal_sum

--- utils/matrix.py
+++ utils/matrix.py
@@ -6,7 +6,7 @@ def elements_sum(matrix):
     :param matrix: matriz cuadrada
     :return: La suma de todos los elementos de la matriz
     """
-    pass
+    return sum([sum(line) for line in matrix])
 
 
 def diagonal_sum(matrix):
@@ -16,4 +16,8 @@ def diagonal_sum(matrix):
     :param matrix: matriz cuadrada
     :return: La suma de los elementos de la diagonal principal de la matriz
     """
-    pass
+    size = len(matrix)
+    sum = 0
+    for i in range(size):
+        sum += matrix[i][i]
+    return sum
